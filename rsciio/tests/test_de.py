9c79d588eb0ce7b6396c993d54a2d66ad406da37
cssfrancis <csfrancis@wisc.edu>
2022-08-11 17:33:01 -0500

    Testing: Cleaned up testing. Small changes to code fixing errors with orientation

--- rsciio/tests/test_de.py
+++ rsciio/tests/test_de.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import pytest
 from hyperspy.io import load
 import numpy as np
-
-from rsciio.de.api import SeqReader,CeleritasReader
+import glob
+from rsciio.de.api import SeqReader,CeleritasReader,file_reader
 
 
 class TestShared:
@@ -17,52 +17,70 @@ class TestShared:
 
     def test_parse_header(self, seq):
         header = seq._read_file_header()
-        assert header["ImageWidth"] == 2048
-        assert header["ImageHeight"] == 2048
+        assert header["ImageWidth"] == 64
+        assert header["ImageHeight"] == 64
         assert header["ImageBitDepthReal"] == 12
         assert header["NumFrames"] == 10
-        assert header["TrueImageSize"] == 8396800
-        np.testing.assert_almost_equal(header["FPS"], 11.111111, 1)  # This value is wrong for the celeritas camera
+        assert header["TrueImageSize"] == 16384
+        np.testing.assert_almost_equal(header["FPS"], 30, 1)  # Note this value is very wrong for Celeritas Camera
+        # Read from the xml file...
 
     def test_parse_metadata(self, seq):
         metadata = seq._read_metadata()
+        print(metadata)
 
     def test_read_dark(self, seq):
-        dark,gain = seq._read_dark_gain()
-        assert gain.shape == dark.shape
+        dark, gain = seq._read_dark_gain()
+        assert dark.shape == (64, 64)
+        assert gain is None
 
-    def test_read(self, seq):
-        seq.read_data()
+    @pytest.mark.parametrize("nav_shape", [None, (5, 2), (5, 3)])
+    def test_read(self, seq, nav_shape):
+        data = seq.read_data(navigation_shape=nav_shape)
+        if nav_shape is None:
+            nav_shape = (10,)
+        assert data["data"].shape == (*nav_shape, 64, 64)
 
 
 class TestLoadCeleritas:
     @pytest.fixture
     def seq(self):
-        return CeleritasReader(file="de_data/celeritas_data/test_SS8.seq",
-                               top="de_data/celeritas_data/test_SS8_Top_14-16-01.432.seq",
-                               bottom="de_data/celeritas_data/test_SS8_Bottom_14-16-01.468.seq",
-                               dark="de_data/celeritas_data/test_SS8.seq.dark.mrc",
-                               gain="de_data/celeritas_data/test_SS8.seq.gain.mrc",
-                               xml="de_data/celeritas_data/test_SS8.seq.Config.Metadata.xml",
-                               metadata="de_data/celeritas_data/test_SS8_Bottom_14-16-01.468.seq.metadata")
+        files = sorted(glob.glob("de_data/celeritas_data/128x256_PRebuffer128/*"))
+        kws = {"file":"de_data/celeritas_data/128x256_PRebuffer128/test.seq",
+               "top": files[6],
+               "bottom":files[4],
+               "dark": files[2],
+               "gain": files[3],
+               "xml": files[0],
+               "metadata": files[7]}
+        return CeleritasReader(**kws)
 
     def test_parse_header(self, seq):
+        print(seq.bottom)
         header = seq._read_file_header()
         assert header["ImageWidth"] == 256
         assert header["ImageHeight"] == 8192
         assert header["ImageBitDepthReal"] == 12
-        assert header["NumFrames"] == 14 #this is wrong
+        assert header["NumFrames"] == 4 #this is wrong
         assert header["TrueImageSize"] == 4202496
-        np.testing.assert_almost_equal(header["FPS"], 309.5, 1)  # This value is wrong for the celeritas camera
+        np.testing.assert_almost_equal(header["FPS"], 300, 1)  # This value is wrong for the celeritas camera
 
     def test_parse_xml(self, seq):
         xml = seq._read_xml()
         assert xml["ImageSizeX"] == 256
-        assert xml["ImageSizeY"] == 256
-        assert xml["FrameRate"] == 20000
+        assert xml["ImageSizeY"] == 128
+        assert xml["FrameRate"] == 40000 # correct FPS
         assert xml["DarkRef"] == "Yes"
         assert xml["GainRef"] == "Yes"
-        assert xml["SegmentPreBuffer"] == 64
+        assert xml["SegmentPreBuffer"] == 128
 
-    def test_read(self, seq):
-        dict = seq.read_data()
+    @pytest.mark.parametrize("nav_shape", [None, (5, 2), (5, 3)])
+    def test_read(self, seq, nav_shape):
+        data_dict = seq.read_data()
+        assert data_dict["data"].shape == (128, 128, 256)
+
+
+def test_load_file():
+    data_dict = file_reader("de_data/celeritas_data/128x256_PRebuffer128/test_Top_14-04-59.355.seq",
+                            celeritas=True)
+    assert data_dict["data"].shape == (128, 128, 256)

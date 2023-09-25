232990f5646b2a0b1c8db98836a912165770cd6e
Carter Francis <csfrancis@wisc.edu>
2023-09-25 11:35:32 -0500

    New Feature: Added function for loading data using distributed computing and the `np.memmap` function.

---
+++ rsciio/utils/distributed.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+# Copyright 2007-2023 The HyperSpy developers
+#
+# This file is part of RosettaSciIO.
+#
+# RosettaSciIO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RosettaSciIO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RosettaSciIO. If not, see <https://www.gnu.org/licenses/#GPL>.
+
+
+import numpy as np
+import dask.array as da
+
+
+def get_chunk_slice(
+    shape,
+    chunks="auto",
+    block_size_limit=None,
+    dtype=None,
+):
+    """Get chunk slices for the slice_memmap function.
+
+    Parameters
+    ----------
+    shape : tuple
+        Shape of the data.
+    chunks : tuple, optional
+        Chunk shape. The default is "auto".
+    block_size_limit : int, optional
+        Maximum size of a block in bytes. The default is None.
+    dtype : numpy.dtype, optional
+        Data type. The default is None.
+    """
+
+    chunks = da.core.normalize_chunks(
+        chunks=chunks, shape=shape, limit=block_size_limit, dtype=dtype
+    )
+    chunks_shape = tuple([len(c) for c in chunks])
+    slices = np.empty(
+        shape=chunks_shape
+        + (
+            len(chunks_shape),
+            2,
+        ),
+        dtype=int,
+    )
+    for ind in np.ndindex(chunks_shape):
+        current_chunk = [chunk[i] for i, chunk in zip(ind, chunks)]
+        starts = [int(np.sum(chunk[:i])) for i, chunk in zip(ind, chunks)]
+        stops = [s + c for s, c in zip(starts, current_chunk)]
+        slices[ind] = [[start, stop] for start, stop in zip(starts, stops)]
+    return da.from_array(slices, chunks=(1,) * len(shape) + slices.shape[-2:]), chunks
+
+
+def slice_memmap(sl, file, dtypes, shape, **kwargs):
+    """Slice a memmaped file using a tuple of slices.
+
+    Parameters
+    ----------
+    sl : array-like
+        An array of the slices to use. The dimensions of the array should be
+        (n,2) where n is the number of dimensions of the data. The first column
+        is the start of the slice and the second column is the stop of the slice.
+    file : str
+        Path to the file.
+    dtypes : numpy.dtype
+        Data type of the data for memmap functino
+    shape : tuple
+        Shape of the data to be read
+    **kwargs : dict
+        Additional keyword arguments to pass to the memmap function.
+    """
+    sl = np.squeeze(sl)[()]
+    data = np.memmap(file, dtypes, shape=shape, **kwargs)
+    slics = tuple([slice(s[0], s[1]) for s in sl])
+    return data[slics]
+
+
+def memmap_distributed(
+    file,
+    dtype,
+    offset=0,
+    shape=None,
+    order="C",
+    chunks="auto",
+    block_size_limit=None,
+):
+    """Drop in replacement for `np.memmap` allowing for distributed loading of data.
+
+    This always loads the data using dask which can be beneficial in many cases, but
+    may not be ideal in others.
+
+    The chunks and block_size_limit are for describing an ideal chunk shape and size
+    as defined using the `da.core.normalize_chunks` function.
+
+    Parameters
+    ----------
+    file : str
+        Path to the file.
+    dtype : numpy.dtype
+        Data type of the data for memmap functino
+    offset : int, optional
+        Offset in bytes. The default is 0.
+    shape : tuple, optional
+        Shape of the data to be read. The default is None.
+    order : str, optional
+        Order of the data. The default is "C" see `np.memmap` for more details.
+    chunks : tuple, optional
+        Chunk shape. The default is "auto".
+    block_size_limit : int, optional
+        Maximum size of a block in bytes. The default is None.
+
+    Returns
+    -------
+    data : dask.array.Array
+        Dask array of the data from the memmaped file and with the specified chunks.
+    """
+    # Separates slices into appropriately sized chunks.
+    chunked_slices, data_chunks = get_chunk_slice(
+        shape=shape,
+        chunks=chunks,
+        block_size_limit=block_size_limit,
+        dtype=dtype,
+    )
+    data = da.map_blocks(
+        slice_memmap,
+        chunked_slices,
+        file=file,
+        dtype=dtype,
+        shape=shape,
+        order=order,
+        mode="r",
+        dtypes=dtype,
+        offset=offset,
+        chunks=data_chunks,
+        key=key,
+        drop_axis=(-1, -2),
+    )
+    return data

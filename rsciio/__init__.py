4a9f6c8b2611893e07384a34f93523778b2053fc
Eric Prestat <eric.prestat@gmail.com>
2023-11-25 11:16:31 +0000

    Fix get version in various scenarios: - debian use `dist-packages` instead of `site-packages` - dev version, opt in to use setuptools_scm when possible, for example for non shallow checkout

--- rsciio/__init__.py
+++ rsciio/__init__.py
@@ -16,25 +16,41 @@
 # You should have received a copy of the GNU General Public License
 # along with RosettaSciIO. If not, see <https://www.gnu.org/licenses/#GPL>.
 
+from importlib.metadata import version
 import logging
 import os
 from pathlib import Path
 import yaml
 
 
-if Path(__file__).parent.parent.name == "site-packages":  # pragma: no cover
-    # Tested in the "build" workflow on GitHub CI
-    from importlib.metadata import version
-
-    __version__ = version("rosettasciio")
-else:
-    # Editable install
-    from setuptools_scm import get_version
-
-    __version__ = get_version(Path(__file__).parent.parent)
-
-IO_PLUGINS = []
 _logger = logging.getLogger(__name__)
+IO_PLUGINS = []
+
+__version__ = version("rosettasciio")
+
+# For development version, `setuptools_scm` will be used at
+# build time to get the dev version:
+# - in case of missing vcs information, it will fallback to the
+#   version defined in pyproject.toml will be used
+# - in case of shallow checkout (pip install git+https://...)
+
+# if we have a editable install from a git repository try to use
+# `setuptools_scm` to find a more accurate version:
+# `importlib.metadata` will provide the version at installation
+# time and for editable version this may be different
+
+# we only do that if we have enough git history, e.g. not shallow checkout
+_root = Path(__file__).resolve().parents[1]
+if (_root / ".git").exists() and not (_root / ".git/shallow").exists():
+    try:
+        # setuptools_scm may not be installed
+        from setuptools_scm import get_version
+
+        __version__ = get_version(_root)
+    except ImportError:  # pragma: no cover
+        # setuptools_scm not install, we keep the existing __version__
+        pass
+
 
 for sub, _, _ in os.walk(os.path.abspath(os.path.dirname(__file__))):
     _specsf = os.path.join(sub, "specifications.yaml")
@@ -44,6 +60,7 @@ for sub, _, _ in os.walk(os.path.abspath(os.path.dirname(__file__))):
             # for testing purposes
             _specs["api"] = "rsciio.%s" % os.path.split(sub)[1]
             IO_PLUGINS.append(_specs)
+
 
 __all__ = [
     "__version__",

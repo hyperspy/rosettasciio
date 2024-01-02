b08c8688a184a49ac36618218381d8f551779b7c
jlaehne <jonas@pdi-berlin.de>
2024-01-02 11:30:16 +0100

    fix hamamatsu signal type

--- rsciio/hamamatsu/_api.py
+++ rsciio/hamamatsu/_api.py
@@ -359,7 +359,7 @@ class IMGReader:
             )
             signal["signal_type"] = ""
         else:
-            signal["signal_type"] = "Luminescence"  # pragma: no cover
+            signal["signal_type"] = "TransientSpec"  # pragma: no cover
 
         try:
             quantity = self.original_metadata["Comment"]["Acquisition"]["ZAxisLabel"]

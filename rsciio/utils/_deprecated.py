7e9ad842d60e57b1db9841b8ad92c7aa53a61e27
Carter Francis <csfrancis@wisc.edu>
2025-05-20 20:54:15 -0500

    Testing: Add deprecation tests

--- rsciio/utils/_deprecated.py
+++ rsciio/utils/_deprecated.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2007-2025 The HyperSpy developers
+#
+# This file is part of RosettaSciIO.
+#
+# RosettaSciIO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RosettaSciIO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RosettaSciIO. If not, see <https://www.gnu.org/licenses/#GPL>.
+
+
 import functools
 import inspect
 from typing import Callable, Optional, Union
@@ -110,11 +129,11 @@ class deprecated_argument:
                 msg = (
                     f"Argument `{self.name}` is deprecated")
                 if self.removal is not None:
-                    msg += f"and will be removed in version {self.removal}."
+                    msg += f" and will be removed in version {self.removal}."
                 else:
                     msg += " and has been removed. "
                     kwargs.pop(self.name)
-                msg += "To avoid this warning, please do not use f`{self.name}`"
+                msg += f" To avoid this warning, please do not use `{self.name}`. "
                 if self.alternative is not None:
                     msg += f"Use `{self.alternative}` instead. "
                     kwargs[self.alternative] = kwargs.pop(self.name)

775df2af4b525768897b6e23e85cc7319d8b04ce
Carter Francis <cartsfrancis@gmail.com>
2025-11-05 15:46:14 -0600

    Enhance MRC file handling: add regex parsing for virtual images and related file discovery

--- rsciio/tests/test_mrc.py
+++ rsciio/tests/test_mrc.py
@@ -140,8 +140,8 @@ def test_mrc_metadata_auto():
     assert s.metadata.Acquisition_instrument.TEM.magnification == "1000"
     assert s.metadata.Acquisition_instrument.TEM.frames_per_second == "700"
     assert len(s.metadata.General.virtual_images.keys()) == 2
-    assert len(s.metadata.General.external_detectors) == 1
-    assert isinstance(s.metadata.General.virtual_images.image_0, hs.signals.Signal2D)
+    assert len(s.metadata.General.external_images.keys()) == 1
+    assert isinstance(s.metadata.General.virtual_images["Virt 0"], hs.signals.Signal2D)
 
     assert s.metadata._HyperSpy.navigator is not None
 

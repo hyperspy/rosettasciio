faa42cf301424bef77d79ee63d826951aed23a01
Gregor Mönke <moenke@ceos-gmbh.de>
2025-12-04 17:40:48 +0100

    Remove unused imports from numpy api
    This is a fixup for 3f30a95

--- rsciio/numpy/_api.py
+++ rsciio/numpy/_api.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with RosettaSciIO. If not, see <https://www.gnu.org/licenses/#GPL>.
 
-import ast
-import struct
 
 import dask.array as da
 import numpy as np

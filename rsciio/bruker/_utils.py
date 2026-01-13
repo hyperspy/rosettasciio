7c1bf8f62d38bdd55f9692b0487fc16bfc1d17ff
jlaehne <jonas@pdi-berlin.de>
2026-01-13 23:21:03 +0100

    update copyright

--- rsciio/bruker/_utils.py
+++ rsciio/bruker/_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007-2025 The HyperSpy developers
+# Copyright 2007-2026 The HyperSpy developers
 #
 # This library is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
